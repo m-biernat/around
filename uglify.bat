433f9776da05bfffb74c90b31039dae56e9465e9
m-bt <michalbiernat@windowslive.com>
2019-01-18 17:43:49 +0100

    Update 180119.1
    Merged game scripts into single minified script game.min.js and changed that in index.html. Now game should load faster.

---
+++ uglify.bat
@@ -0,0 +1,10 @@
+uglifyjs^
+ scripts\load.js^
+ scripts\menu.js^
+ scripts\play\planet.js^
+ scripts\play\player.js^
+ scripts\play\asteroid.js^
+ scripts\play.js^
+ scripts\lose.js^
+ scripts\game.js^
+ --compress --mangle --output game.min.js
